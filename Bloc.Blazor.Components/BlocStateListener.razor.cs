416d83a774e52e60743adb9fd9f868bedb485ed6
Abdulmohsen Alkhamees <vaygeth@gmail.com>
2023-07-24 13:25:53 +0300

    renamed

--- Bloc.Blazor.Components/BlocStateListener.razor.cs
+++ Bloc.Blazor.Components/BlocStateListener.razor.cs
@@ -6,7 +6,7 @@ namespace Bloc.Blazor.Components;
 public partial class BlocStateListener<TBloc, TState> : ComponentBase
     where TBloc : BlocBase<TState> where TState : BlocState
 {
-    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public RenderFragment? ChildContent { get; set; } = default!;
     [Inject] public BlocBuilder<TBloc, TState> Builder { get; set; }
     [Parameter] public EventCallback<TState> OnInitializeState { get; set; }
     [Parameter] public EventCallback<TState> OnStateChange { get; set; }
