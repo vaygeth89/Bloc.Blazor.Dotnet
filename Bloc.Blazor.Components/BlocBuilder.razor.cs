26d0465f842fab9f80cf87894b71f8d650ca2ac0
Abdulmohsen Alkhamees <vaygeth@gmail.com>
2023-07-24 12:08:24 +0300

    initial

---
+++ Bloc.Blazor.Components/BlocBuilder.razor.cs
@@ -0,0 +1,25 @@
+﻿using Bloc.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Bloc.Blazor.Components;
+
+public partial class BlocStateListener<TBloc, TState> : ComponentBase
+    where TBloc : BlocBase<TState> where TState : BlocState
+{
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Inject] public BlocBuilder<TBloc, TState> Builder { get; set; }
+    [Parameter] public EventCallback<TState> OnInitializeState { get; set; }
+    [Parameter] public EventCallback<TState> OnStateChange { get; set; }
+
+    protected override void OnInitialized()
+    {
+        Builder.Bloc.OnStateChanged += ListenToChanges;
+        OnInitializeState.InvokeAsync(Builder.State);
+        base.OnInitialized();
+    }
+
+    private async void ListenToChanges(TState state)
+    {
+        await OnStateChange.InvokeAsync(state);
+    }
+}
